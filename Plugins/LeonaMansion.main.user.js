5f54153b678d41d4c5aaef92c5c5fadcba1dac58
Leona-BC <crawd127@gmail.com>
2026-08-02 10:11:42 -0400

    Update LeonaMansion.main.user.js

--- Plugins/LeonaMansion.main.user.js
+++ Plugins/LeonaMansion.main.user.js
@@ -176,6 +176,10 @@
         menuButton.innerText = 'Start Fishing';
         const style = document.createElement("style");
         style.textContent = `.menuButton { position: absolute; top: 20px; left: 20px; z-index: 9999;}`;
+        menuButton.style.position = "absolute";
+        menuButton.style.top = "20px";
+        menuButton.style.left = "20px";
+        menuButton.style.zIndex = "9999";
         document.body.append(menuButton);
         document.getElementById("StartFishing").onclick = () => createFishingPopup();
     }
