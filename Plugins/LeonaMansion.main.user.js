dddbc94e1c104964ccca2bebe68e440805cf487e
Leona-BC <crawd127@gmail.com>
2026-08-03 07:29:54 -0400

    Update LeonaMansion.main.user.js

--- Plugins/LeonaMansion.main.user.js
+++ Plugins/LeonaMansion.main.user.js
@@ -65,7 +65,7 @@
     }
 
     function removeButton() {
-        document.querySelector(".CleanDishes")?.remove();
+        document.querySelector(".StartFishing")?.remove();
     }
 
     async function mainThread() {
