c2b0a69cfb09355ab2b79aa26a1e5417bdb5d917
Leona-BC <crawd127@gmail.com>
2026-08-07 07:07:03 -0400

    Update CleanDishes.js

--- Plugins/MiniGames/CleanDishes.js
+++ Plugins/MiniGames/CleanDishes.js
@@ -119,14 +119,6 @@ function startDishesCleaningMiniGame(trembleLevel = 0) {
         visible: false
     };
 
-    // --- Center canvas AFTER paint ---
-    requestAnimationFrame(() => {
-        canvas.style.left = "-600px";
-        canvas.style.top = "-200px";
-        gameWindow.style.left = "-600px";
-        gameWindow.style.top = "-200px";
-    });
-
     // -------------------------------
     // Init State
     // -------------------------------
